9de1880e1b5d852189bb27390ccf04f27a42c037
axelbunt <49002597+axelbunt@users.noreply.github.com>
2021-10-31 14:05:05 +0300

    Изменен ряд функций
    1) Изменена генерация комбинации
    2) Название get_guess изменено на более понятное
    3) Добавлен перехват исключений в функцию main

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ import sys
 from typing import Callable
 
 
-# todo: ограничить колво возможных цифр (всего цветов 6)
+# todo: ограничить колво возможных цифр (всего цветов 6) - Done
 def setup_new_secret_num(required_len_of_num: int) -> str:
-    return str(randint(10 ** (required_len_of_num - 1), 10 ** required_len_of_num - 1))
+    # return str(randint(10 ** (required_len_of_num - 1), 10 ** required_len_of_num - 1))
+    secret_num = ''
+    for _ in range(required_len_of_num):
+        secret_num += str(randint(1, 6))
+    return secret_num
 
 
 assert len(setup_new_secret_num(4)) == 4, setup_new_secret_num(4)
@@ -35,7 +39,7 @@ def grader(guess: str, secret_num: str) -> Grade:
     return Grade(black, white)
 
 
-def get_guess(num_of_turns: int, secret_num: str, ui: Ui) -> Grade:
+def get_grade(num_of_turns: int, secret_num: str, ui: Ui) -> Grade:
     secret_num_len = len(secret_num)
 
     guess = ui.get_correct_input(num_of_turns, secret_num_len)
@@ -47,38 +51,38 @@ def get_guess(num_of_turns: int, secret_num: str, ui: Ui) -> Grade:
 
 
 def main(turn_count: int, required_len_of_secret_num: int,
-         ui_type: Callable[[int, int], Ui]) -> None:
-    # ui = Console(turn_count, required_len_of_secret_num)
-    ui = ui_type(turn_count, required_len_of_secret_num)
-
+         ui_type: Callable[[int, str], Ui]) -> None:
     num_of_turn = 1
     secret_num = setup_new_secret_num(required_len_of_secret_num)
     game_win = False
 
-    ui.start_game()
-    # ui.print_secret_num_size(secret_num)
+    ui = ui_type(turn_count, secret_num)
 
-    grade = get_guess(num_of_turn, secret_num, ui)
-    if grade.black == 4:
-        game_win = True
-    else:
-        num_of_turn += 1
-        while num_of_turn <= turn_count:
-            grade = get_guess(num_of_turn, secret_num, ui)
-
-            if grade.black == 4:
-                game_win = True
-                break
-
+    try:
+        ui.start_game()
+        grade = get_grade(num_of_turn, secret_num, ui)
+        if grade.black == 4:
+            game_win = True
+        else:
             num_of_turn += 1
+            while num_of_turn <= turn_count:
+                grade = get_grade(num_of_turn, secret_num, ui)
 
-    ui.rate_player(game_win, num_of_turn)
+                if grade.black == 4:
+                    game_win = True
+                    break
+
+                num_of_turn += 1
+
+        ui.rate_player(game_win, num_of_turn)
+    except Exception as e:
+        print(e)
 
 
 TURN_COUNT = 10
 LEN_OF_SECRET_NUM = 4
 if __name__ == '__main__':
-    ui_constructor: Callable[[int, int], Ui] = Gui
+    ui_constructor: Callable[[int, str], Ui] = Gui
 
     if "Console" in sys.argv:
         ui_constructor = Console
